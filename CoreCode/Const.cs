ca3f04af43658ffc4f90d9f55d2dbf856618fe9b
yvan.zhang <yvan.zhang@dji.com>
2018-11-05 21:14:59 +0800

    提交

--- CoreCode/Const.cs
+++ CoreCode/Const.cs
@@ -3,13 +3,13 @@ public class Const{
 /// <summary>
 /// AccessKey
 /// </summary>
-public static  string  AccessKey {get{return "vCUMX9O-py4b0s1aozx28NR-JZF9NfbOG-VHctnB";}}
+public static  string  AccessKey {get{return "vCUMX9O-py4b0s1aozx28NR-JZF9NfbOG-VHctnC";}}
 
 /// <summary>
 /// SecretKey
 /// </summary>
-public static  string  SecretKey {get{return "dISKTS28xjBxcU4Hfzi22PxqeQFkGkM4Gh4AyBTT";}}
+public static  string  SecretKey {get{return "dISKTS28xjBxcU4Hfzi22PxqeQFkGkM4Gh4AyBTU";}}
 
 
-public static string BucketName{get{return "20161022";}}
+public static string BucketName{get{return "20161023";}}
 }
